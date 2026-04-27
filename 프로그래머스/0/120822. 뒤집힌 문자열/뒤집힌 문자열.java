b392c63ca5e513ee560d7b3ac70177da7edb3f07
Hyeonji Kang <162887985+hz00308@users.noreply.github.com>
2026-04-28 08:49:09 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.04 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder(my_string).reverse();
+        String answer = sb.toString();
+        return answer;
+    }
+}
