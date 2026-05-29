f0df67173d09ee7b40e096a72c7abc3f51d01dc5
Hyeonji Kang <162887985+hz00308@users.noreply.github.com>
2026-05-29 17:04:24 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -0,0 +1,8 @@
+select 
+    rest_id, rest_name, food_type, favorites, address, 
+    round(avg(review_score), 2) as score
+from rest_info join rest_review using(rest_id)
+where address like '서울%'
+group by rest_id, rest_name, food_type, favorites, address
+order by score desc, favorites desc;
+
